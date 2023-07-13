4e60248721c5989e868e3744ce923ac9152c1a94
Karibasaveshwara_TG <karibasaveshwara.tg@moonraft.com>
2023-07-13 11:45:41 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OneComponent } from './one/one.component';
+import { TwoComponent } from './two/two.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, OneComponent, TwoComponent, NotfoundComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [FormsModule, MatFormFieldModule, MatInputModule],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/two/two.component.ts
@@ -0,0 +1,80 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-two',
+  templateUrl: './two.component.html',
+  styleUrls: ['./two.component.css'],
+})
+export class TwoComponent {
+  otp: string = '';
+  number = 0;
+  @ViewChild('oref', { static: false }) otpRef: ElementRef<HTMLElement> | any;
+  otpInputFields = {} as any;
+
+  ngOnInit(): void {
+    this.onSubmit();
+    this.resetOtpFields();
+  }
+
+  submit() {
+    alert(this.otpInputFields.intakeReal);
+    this.otpInputFields.intakeReal = [];
+    this.otpInputFields.intakeMasked = [];
+  }
+
+  onSubmit() {
+    this.number++;
+    if ('OTPCredential' in window) {
+      this.number++;
+      const ac = new AbortController();
+      const form = document.querySelector('form');
+      if (form) {
+        form.addEventListener('submit', (e) => {
+          ac.abort();
+        });
+      }
+      const otpCode: any = navigator.credentials;
+      otpCode
+        .get({
+          otp: { transport: ['sms'] },
+          signal: ac.signal,
+        })
+        .then((otp: any) => {
+          this.number++;
+          this.otp = otp.code;
+          if (form) form.submit();
+        })
+        .catch((err: any) => {
+          console.log(err);
+        });
+    }
+  }
+
+  resetOtpFields() {
+    const sudoApp = ['', '', '', '', ''];
+    this.otpInputFields.intakeReal = [];
+    this.otpInputFields.intakeMasked = [];
+    sudoApp.forEach((item) => {
+      this.otpInputFields.intakeMasked.push(item);
+    });
+  }
+
+  tabTrigger($event: any, $index: any) {
+    const inputValue = $event.target.value;
+
+    if (inputValue !== '*') {
+      if (inputValue !== '') {
+        this.otpInputFields.intakeReal[$index] = inputValue;
+        this.otpInputFields.intakeMasked[$index] = '*';
+        if ($index < 4) {
+          this.otpRef.nativeElement[$index + 1].focus();
+        }
+      } else {
+        // if not backspace
+        // we rely on html 5 here to make sure we only get numeric values of single digits [0-9]
+        // and focus on the first child
+        this.otpRef.nativeElement[0].focus();
+      }
+    }
+  }
+}
